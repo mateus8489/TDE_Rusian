90af19706f5622a25fb7589ab2d6fa448e1f3de8
mateus8489 <86335406+mateus8489@users.noreply.github.com>
2021-10-11 21:16:17 -0300

    Nova class adicionada (Cliente) com metodos get e set

---
+++ syscorreios/src/main/java/br/com/correiosframe/cliente/Cliente.java
@@ -0,0 +1,40 @@
+package br.com.correiosframe.cliente;
+
+public class Cliente {
+
+	private String cep;
+	private String logradouro;
+	private String bairro;
+	private String municipio;
+	private String uf;
+	public String getCep() {
+		return cep;
+	}
+	public void setCep(String cep) {
+		this.cep = cep;
+	}
+	public String getLogradouro() {
+		return logradouro;
+	}
+	public void setLogradouro(String logradouro) {
+		this.logradouro = logradouro;
+	}
+	public String getBairro() {
+		return bairro;
+	}
+	public void setBairro(String bairro) {
+		this.bairro = bairro;
+	}
+	public String getMunicipio() {
+		return municipio;
+	}
+	public void setMunicipio(String municipio) {
+		this.municipio = municipio;
+	}
+	public String getUf() {
+		return uf;
+	}
+	public void setUf(String uf) {
+		this.uf = uf;
+	}
+}
