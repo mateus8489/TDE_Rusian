36968d254c2e8e89c8f357c4de6fc4ed7b21225a
mateus8489 <86335406+mateus8489@users.noreply.github.com>
2021-10-12 20:55:24 -0300

    plugin

--- syscorreios/target/generated-sources/xjc/com/linecode/jcep/wsdl/package-info.java
+++ syscorreios/target/generated-sources/xjc/com/linecode/jcep/wsdl/package-info.java
@@ -2,7 +2,7 @@
 // Este arquivo foi gerado pela Arquitetura JavaTM para Implementação de Referência (JAXB) de Bind XML, v2.3.0 
 // Consulte <a href="https://javaee.github.io/jaxb-v2/">https://javaee.github.io/jaxb-v2/</a> 
 // Todas as modificações neste arquivo serão perdidas após a recompilação do esquema de origem. 
-// Gerado em: 2021.10.12 às 09:43:54 AM BRT 
+// Gerado em: 2021.10.12 às 08:43:57 PM BRT 
 //
 
 @javax.xml.bind.annotation.XmlSchema(namespace = "http://cliente.bean.master.sigep.bsb.correios.com.br/")
--- syscorreios/target/generated-test-sources/xjc/com/linecode/jcep/wsdl/SimNao.java
+++ syscorreios/target/generated-test-sources/xjc/com/linecode/jcep/wsdl/SimNao.java
@@ -2,7 +2,7 @@
 // Este arquivo foi gerado pela Arquitetura JavaTM para Implementação de Referência (JAXB) de Bind XML, v2.3.0 
 // Consulte <a href="https://javaee.github.io/jaxb-v2/">https://javaee.github.io/jaxb-v2/</a> 
 // Todas as modificações neste arquivo serão perdidas após a recompilação do esquema de origem. 
-// Gerado em: 2021.10.12 às 09:43:55 AM BRT 
+// Gerado em: 2021.10.12 às 08:43:58 PM BRT 
 //
 
 
